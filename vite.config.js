a6ea0d60452bd8167e8db355491eaa58dcc0fe84
Martinonyte Dovile <71691245+dovile-mart@users.noreply.github.com>
2025-04-24 15:09:49 +0300

    Deployment test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/dovile-mart/vueTest.git',
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
